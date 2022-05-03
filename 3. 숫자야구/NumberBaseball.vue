2694da761f8a1ee0257b178156a4426f04680e36
jhjh2 <jhjh2>
2022-05-03 21:59:45 +0900

    first commit

---
+++ 3. 숫자야구/NumberBaseball.vue
@@ -0,0 +1,88 @@
+<template>
+    <div>
+        <h1>{{result}}</h1>
+        <form v-on:submit.prevent="onSubmitForm">
+            <input ref="answer" maxlength="4" v-model="value" />
+            <button type="submit">입력</button>
+        </form>
+        <div>시도 : {{tries.length}}</div>
+        <ul>
+            <li v-for="t in tries" :key="t">
+                <div>{{t.try}}</div>
+                <div>{{t.result}}</div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    const getNumbers = () => {
+        const candidates = [1,2,3,4,5,6,7,8,9];
+        const array = [];
+        for (let i = 0; i < 4; ++i){
+            const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+            array.push(chosen);
+        }
+    }
+
+    export default {
+        data(){
+            return {
+                answer: getNumbers(),
+                tries: [],
+                value: '',
+                result: '',
+            };
+        },
+        methods: {
+            onSubmitForm(e){
+                //e.preventDefault();
+                if(this.value === this.answer.join('')){
+                    this.tries.push({
+                        try: this.value,
+                        result: '홈런',
+                    });
+                    this.result = '홈런';
+                    alert('게임을 다시 실행합니다.');
+                    this.value = '';
+                    this.answer.getNumbers();
+                    this.tries = [];
+                    this.$refs.answer.focus();
+                } else { // 정답 틀렸을 때
+                    if(this.tries.length >= 9){ // 10번 째 시도
+                        this.result = `실패! 정답은 ${this.answer.join(',')}입니다.`;
+                        alert('게임을 다시 실행합니다.');
+                        this.value = '';
+                        this.answer.getNumbers();
+                        this.tries = [];
+                        this.$refs.answer.focus();
+                    }
+                    let striek = 0; // 화면에 보이지 않는 데이터는 변수로 선언
+                    let ball = 0; // 화면에 보이는 데이터는 data()에 선언
+                    const answerArray = this.value.split('').map(v => parseInt(v));
+                    for(let i = 0; i <4; ++i){
+                        if(answerArray[i] === this.answer[i]) { // 숫자와 자릿수 모두 정답
+                            strike++;
+                        } else if(this.answer.includes(answerArray[i])) { // 숫자만 정답
+                            ball++;
+                        }
+                    }
+                    this.tries.push({
+                        try: this.value,
+                        result: `${strike} 스트라이트, ${ball} 볼입니다.`,
+                    });
+                    this.value='';
+                    this.$refs.answer.focus();
+                }
+
+                
+                
+            }
+        },
+
+    };
+</script>
+
+<style>
+
+</style>
---
+++ 3. 숫자야구/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue' // ES2015 모듈시스템
+import NumberBaseball from './numberbaseball.vue';
+
+new Vue(NumberBaseball).$mount('#root'); // Vue 인스턴스
